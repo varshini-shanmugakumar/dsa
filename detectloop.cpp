5f24b0f0625af733d94148b234828a5a0f9fc34c
varshini-shanmugakumar <87574552+varshini-shanmugakumar@users.noreply.github.com>
2022-08-07 12:36:52 +0530

    Create detectloop.cpp

---
+++ detectloop.cpp
@@ -0,0 +1,13 @@
+bool detectLoop(Node* head)
+    {
+        Node* slow=head;
+        Node* fast=head;
+        
+        while(fast!=NULL and fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
